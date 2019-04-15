82045c8b5ccaed3edb6601846bab5af50331db1e
vincent <vincentgong3mm@gmail.com>
2019-04-15 10:12:09 +0000

    test tui

---
+++ tuitest/tuimain.go
@@ -0,0 +1,41 @@
+package main
+
+import tl "github.com/JoelOtter/termloop"
+
+type Player struct {
+	*tl.Entity
+}
+
+func (player *Player) Tick(event tl.Event) {
+	if event.Type == tl.EventKey { // Is it a keyboard event?
+		x, y := player.Position()
+		switch event.Key { // If so, switch on the pressed key.
+		case tl.KeyArrowRight:
+			player.SetPosition(x+1, y)
+		case tl.KeyArrowLeft:
+			player.SetPosition(x-1, y)
+		case tl.KeyArrowUp:
+			player.SetPosition(x, y-1)
+		case tl.KeyArrowDown:
+			player.SetPosition(x, y+1)
+		}
+	}
+}
+
+func main() {
+	game := tl.NewGame()
+	level := tl.NewBaseLevel(tl.Cell{
+		Bg: tl.ColorGreen,
+		Fg: tl.ColorBlack,
+		Ch: 'v',
+	})
+	level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
+
+	player := Player{tl.NewEntity(1, 1, 1, 1)}
+	// Set the character at position (0, 0) on the entity.
+	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '@'})
+	level.AddEntity(&player)
+
+	game.Screen().SetLevel(level)
+	game.Start()
+}
