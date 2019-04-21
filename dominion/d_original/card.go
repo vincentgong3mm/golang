64f71f02c3025578682b2534af3bfd2df6c39449
vincent <vincentgong3mm@gmail.com>
2019-04-21 14:49:12 +0900

    test card

--- dominion/d_original/card.go
+++ dominion/d_original/card.go
@@ -23,8 +23,16 @@ func init() {
 	fmt.Println("import d_original/card")
 }
 
+/*
 func CreateNewCard(name string) *Card {
 	nc := Card{name: name}
 
 	return &nc
+}
+*/
+
+func CreateNewCard(name string, i CardUnique, cardType []CardType, cost int) *Card {
+	n := Card{name: name, cardUnique: i, cardType: cardType, cost: cost}
+
+	return &n
 }
--- dominion/d_original/d_original_test.go
+++ dominion/d_original/d_original_test.go
@@ -29,8 +29,13 @@ func TestMain(t *testing.T) {
 func TestGameBox(t *testing.T) {
 	gb := dom.CreateNewGameBox()
 
-	nc := dom.CreateNewCard("Village")
+	nc := dom.CreateNewCard("Village",
+		10001, []dom.CardType{dom.CardTypeAction}, 3)
+	nc2 := dom.CreateNewCard("Smithy",
+		10002, []dom.CardType{dom.CardTypeAction}, 3)
+
 	gb.CreateCard(*nc)
+	gb.CreateCard(*nc2)
 
 	fmt.Println(gb)
 
